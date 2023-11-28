fe0a36f308f17ecfb6d4cce8ac1ca9a1084ff092
Roger Viñas <roger.vinas@fastzink.com>
2023-11-28 18:32:03 +0100

    Testcontainers 1.19.3

--- src/test/kotlin/com/rogervinas/kafkastreams/helper/DockerComposeContainerHelper.kt
+++ src/test/kotlin/com/rogervinas/kafkastreams/helper/DockerComposeContainerHelper.kt
@@ -1,37 +1,37 @@
 package com.rogervinas.kafkastreams.helper
 
-import org.testcontainers.containers.DockerComposeContainer
-import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.containers.ComposeContainer
+import org.testcontainers.containers.wait.strategy.Wait.forListeningPort
+import org.testcontainers.containers.wait.strategy.Wait.forLogMessage
 import org.testcontainers.containers.wait.strategy.WaitAllStrategy
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy.Mode.WITH_INDIVIDUAL_TIMEOUTS_ONLY
 import java.io.File
-
-private const val KAFKA = "kafka"
-private const val KAFKA_PORT = 9094
-private const val ZOOKEEPER = "zookeeper"
-private const val ZOOKEEPER_PORT = 2181
 
 class DockerComposeContainerHelper {
 
-  fun createContainer(): DockerComposeContainer<Nothing> {
-    return DockerComposeContainer<Nothing>(File("docker-compose.yml"))
-      .apply { withLocalCompose(true) }
-      .apply {
-        withExposedService(
-          KAFKA,
-          KAFKA_PORT,
-          WaitAllStrategy(WaitAllStrategy.Mode.WITH_INDIVIDUAL_TIMEOUTS_ONLY)
-            .apply { withStrategy(Wait.forListeningPort()) }
-            .apply { withStrategy(Wait.forLogMessage(".*creating topics.*", 1)) }
-        )
-      }
-      .apply {
-        withExposedService(
-          ZOOKEEPER,
-          ZOOKEEPER_PORT,
-          WaitAllStrategy(WaitAllStrategy.Mode.WITH_INDIVIDUAL_TIMEOUTS_ONLY)
-            .apply { withStrategy(Wait.forListeningPort()) }
-            .apply { withStrategy(Wait.forLogMessage(".*binding to port.*", 1)) }
-        )
-      }
+  companion object {
+    private const val KAFKA = "kafka"
+    private const val KAFKA_PORT = 9094
+    private const val ZOOKEEPER = "zookeeper"
+    private const val ZOOKEEPER_PORT = 2181
+  }
+
+  fun createContainer(): ComposeContainer {
+    return ComposeContainer(File("docker-compose.yml"))
+      .withLocalCompose(true)
+      .withExposedService(
+        KAFKA,
+        KAFKA_PORT,
+        WaitAllStrategy(WITH_INDIVIDUAL_TIMEOUTS_ONLY)
+          .withStrategy(forListeningPort())
+          .withStrategy(forLogMessage(".*creating topics.*", 1))
+      )
+      .withExposedService(
+        ZOOKEEPER,
+        ZOOKEEPER_PORT,
+        WaitAllStrategy(WITH_INDIVIDUAL_TIMEOUTS_ONLY)
+          .withStrategy(forListeningPort())
+          .withStrategy(forLogMessage(".*binding to port.*", 1))
+      )
   }
 }
